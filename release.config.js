b797223c3c8219146107c4d11a8f42675266f8da
furiozo <nikolay.panayotov@eldersoss.com>
2022-07-01 13:45:46 +0300

    asdasd

---
+++ release.config.js
@@ -0,0 +1,31 @@
+module.exports={
+  plugins: [
+    ["@semantic-release/commit-analyzer", {
+        releaseRules: [
+            {"type": "major"  , "release": "major"},
+            {"type": "release", "release": "major"},
+        ],
+        parserOpts: {
+            "noteKeywords": ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+        }
+    }],
+
+    ["@semantic-release/exec",{
+        prepareCmd: `CR=unicominternal.azurecr.io && VER=\${nextRelease.version} \
+         && echo \${nextRelease.version} \
+        `,
+        successCmd: `\
+            sed -i 's/{{version}}/\${nextRelease.version}/g' ci/manifest.yaml \
+        `,
+    }],
+
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/git"
+  ],
+
+  branches: [
+    'master',
+    { name: 'release-*', prerelease: true },
+  ],
+}
