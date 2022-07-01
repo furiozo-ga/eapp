1ab60d7735589e7f34de1181acfe6c510bda5196
furiozo <nikolay.panayotov@eldersoss.com>
2022-07-01 18:16:15 +0300

    major: asdasd

--- release.config.js
+++ release.config.js
@@ -30,7 +30,7 @@ module.exports={
             if [ "$TYPE" = "major" ] ; then
                 STRAT=Recreate
             fi
-            sed -i 's/{{strategy}}/$STRAT/g' ci/k8s.yaml
+            sed -i "s/{{strategy}}/$STRAT/g" ci/k8s.yaml
 
             echo "##vso[task.setvariable variable=newVer;]yes"
             echo "##vso[task.setvariable variable=relType;]$TYPE"
